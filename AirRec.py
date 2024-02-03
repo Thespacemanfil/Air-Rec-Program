a8ab05ab43c3d915e54f69077f9fea07a601883a
Thespacemanfil <97069887+Thespacemanfil@users.noreply.github.com>
2024-02-03 22:09:54 +0000

    extra readability

--- AirRec.py
+++ AirRec.py
@@ -1,35 +1,72 @@
-from bing_image_downloader import downloader
-import random, os, glob
+import glob, os, random
 from PIL import Image, ImageTk
 import tkinter as tk
 from tkinter import ttk
+from bing_image_downloader import downloader
 
 def menu():
+    settings = {
+        "path": "C:/aircraft_recognition_program/images/",
+        "slideshow_length": 30,
+        "slideshow_time": 10,
+        "instant_reveal": False,
+        "intermission_time": 0,
+        "variance": 2,
+        "txt_file": "Competition.txt",
+        "text_size": 50,
+        "extension": " airplane",
+        "timer": True,
+    }
+
     with open('paths.txt', 'w'): pass
     print("---Aircraft Recognition Program---")
+
     match input("Guide or slideshow?\n").lower():
+        case "Guide":
+            print("https://aviationgeeks.co.uk/air-rec/air-cadet-list/")
+        case "test":
+            settings.update({
+            "slideshow_length": 3,
+            "slideshow_time": 3,
+            "instant_reveal": True,
+            "intermission_time": 2,
+            })
+            slideshow(**settings)
         case "slideshow":
-            match input("competition, casual, learn or custom mode?\n").lower():
-                case "competition": path = "C:/aircraft_recognition_program/images/"; slideshow_length = 30; slideshow_time = 10; instant_reveal = False; intermission_time = 0; variance = 2; txt_file = "Competition.txt"; text_size = 50; extension = " airplane"; timer = True; slideshow(path,slideshow_length,slideshow_time,instant_reveal,intermission_time,variance,txt_file,text_size,extension,timer)
-                case "casual": path = "C:/aircraft_recognition_program/images/"; slideshow_length = 20; slideshow_time = 10; instant_reveal = False; intermission_time = 5; variance = 2; txt_file = "Competition.txt"; text_size = 50; extension = " aircraft"; timer = True; slideshow(path,slideshow_length,slideshow_time,instant_reveal,intermission_time,variance,txt_file,text_size,extension,timer)
-                case "learn": path = "C:/aircraft_recognition_program/images/"; slideshow_length = 10; slideshow_time = 999; instant_reveal = True; intermission_time = 0; variance = 2; txt_file = "Competition.txt"; text_size = 50; extension = " aircraft"; timer = False; slideshow(path,slideshow_length,slideshow_time,instant_reveal,intermission_time,variance,txt_file,text_size,extension,timer)
-                case _:
-                    path = "C:/aircraft_recognition_program/images/";
-                    slideshow_length = int(input("Amount of slides? -1 slides will run through all aircraft in the list\n"))
-                    slideshow_time = int(input("Seconds per slide?\n"))
-                    if input("Reveal answers immediately yes/no\n").lower() == "yes": instant_reveal = True
-                    else: instant_reveal = False
-                    intermission_time = int(input("Seconds of intermission?\n"))
-                    variance = 3
-                    text_size = 50
-                    txt_file = input("Which list of aircraft do you want to draw from? " + str(glob.glob("*.txt")) + "\n")
-                    extension = " " + input("Search modifier? (not necessary) e.g real aircraft, top view\n").rstrip()
-                    if input("Visible countdown timer yes/no\n").lower() == "yes": timer = True
-                    else: timer = False
-                    slideshow(slideshow_length,slideshow_time,instant_reveal,intermission_time,variance,txt_file,text_size,extension,timer)
-        case "test": path = "C:/aircraft_recognition_program/images/"; slideshow_length = 3; slideshow_time = 3; instant_reveal = True; intermission_time = 2; variance = 2; txt_file = "basic.txt"; text_size = 50; extension = " aircraft"; timer = True; slideshow(path,slideshow_length,slideshow_time,instant_reveal,intermission_time,variance,txt_file,text_size,extension,timer)
-        case "Guide": print("https://aviationgeeks.co.uk/air-rec/air-cadet-list/")
+            slideshowmenu(settings)
         case _: menu()
+    
+def slideshowmenu(settings):
+    match input("competition, casual, learn or custom mode?\n").lower():
+        case "competition":
+            pass  # No changes needed, defaults are for competition mode
+        case "casual":
+            settings.update({
+                "slideshow_length": 20,
+                "intermission_time": 5,
+                "extension": " aircraft",
+            })
+        case "learn":
+            settings.update({
+                "slideshow_length": 10,
+                "slideshow_time": 999,
+                "instant_reveal": True,
+                "timer": False,
+                "extension": " aircraft",
+            })
+        case "custom":
+            settings.update({
+                "slideshow_length": int(input("Amount of slides? -1 slides will run through all aircraft in the list\n")),
+                "slideshow_time": int(input("Seconds per slide?\n")),
+                "instant_reveal": input("Reveal answers immediately yes/no\n").lower() == "yes",
+                "intermission_time": int(input("Seconds of intermission?\n")),
+                "variance": 3,
+                "txt_file": input("Which list of aircraft do you want to draw from? " + str(glob.glob("*.txt")) + "\n"),
+                "extension": " " + input("Search modifier? (not necessary) e.g real aircraft, top view\n").rstrip(),
+                "timer": input("Visible countdown timer yes/no\n").lower() == "yes",
+            })
+
+    slideshow(**settings)
 
 def slideshow(path,slideshow_length,slideshow_time,instant_reveal,intermission_time,variance,txt_file,text_size,extension,timer):
     selected_aircraft = aircraft_selector(txt_file,slideshow_length)
@@ -54,7 +91,7 @@ def image_downloader(selected_aircraft,extension,path,variance):
         query = aircraft + extension
         downloader.download(query, limit=variance, output_dir=path, adult_filter_off=False, force_replace=False, timeout=60, filter="photo", verbose=False)
         
-def show_image(remaining_time,timer,instant_reveal,text_size,filename,image_path,intermission):
+def show_image(remaining_time,timer,instant_reveal,text_size,filename,image_path,intermission,intermission_time):
     root = tk.Tk()
     root.title("Aircraft Image")
     w, h = root.winfo_screenwidth(), root.winfo_screenheight()
@@ -80,7 +117,7 @@ def show_image(remaining_time,timer,instant_reveal,text_size,filename,image_path
         if instant_reveal:
             if intermission:
                 aircraft_label.place(x=w/2, y=h/2, anchor="center")
-            else:
+            elif intermission_time == 0:
                 aircraft_label.place(x=w/2, y=35, anchor="center")
         else:
             aircraft_label.place_forget()
@@ -107,51 +144,61 @@ def show_image(remaining_time,timer,instant_reveal,text_size,filename,image_path
 
 def run_slideshow(slideshow_time, path, text_size, timer, instant_reveal, selected_aircraft, intermission_time, extension):
     for aircraft in selected_aircraft:
-        remaining_time = slideshow_time
-        folder_path = os.path.join(path + aircraft + extension) # get the folder path for each aircraft
-        images = [f for f in os.listdir(folder_path) if f.endswith(".png") or f.endswith(".jpg") or f.endswith(".jpeg")] # list the image files in the folder
-        if not images: print(f"Image not found: {aircraft}"); break
-        random_image = random.choice(images) # pick a random image
-        image_path = os.path.join(folder_path, random_image) # get the full image path
+        folder_path = os.path.join(path, aircraft + extension) # get the folder path for each aircraft
+        images = [f for f in os.listdir(folder_path) if os.path.splitext(f)[1] in (".png", ".jpg", ".jpeg")] # list the image files in the folder
+        if not images: 
+            print(f"Image not found: {aircraft}")
+            continue
+
+        image_path = os.path.join(folder_path, random.choice(images)) # get the full image path
         with open("paths.txt", "a") as f:
             f.write(image_path + "\n")
         
-        show_image(remaining_time, timer, instant_reveal, text_size, aircraft, image_path, False)
+        show_image(slideshow_time, timer, instant_reveal, text_size, aircraft, image_path, False, intermission_time)
         
-        if intermission_time > 0: show_image(intermission_time, timer, instant_reveal, text_size, aircraft, image_path, True)
+        if intermission_time > 0: 
+            show_image(intermission_time, timer, instant_reveal, text_size, aircraft, image_path, True, intermission_time)
 
 def open_image(photo_references, image_path, aircraft_name):
-    root = tk.Toplevel()  # Use Toplevel instead of Tk
-    root.title(f"{aircraft_name} - Image Viewer")
-
     try:
+        root = tk.Toplevel()  # Use Toplevel instead of Tk
+        root.title(f"{aircraft_name} - Image Viewer")
+
         image = Image.open(image_path)
+
+        new_width = int(root.winfo_screenwidth() * 0.75)
+        new_height = int(root.winfo_screenheight() * 0.75)
+        image = image.resize((new_width, new_height), Image.LANCZOS)
+
         photo = ImageTk.PhotoImage(image)
+
+        # Display the image
         label = tk.Label(root, image=photo)
         label.image = photo  # Keep a reference to the image to prevent garbage collection
         label.pack()
-
         # Store photo reference globally
         photo_references.append(photo)
-
         # Display the aircraft name
-        aircraft_label = tk.Label(root, text=aircraft_name, font=('Arial', 14))
+        aircraft_label = tk.Label(root, text=aircraft_name, font=('Arial', 30))
         aircraft_label.pack()
 
-    except Exception as e: print(f"Error: {e}")
+        root.mainloop()
 
-    root.mainloop()
+    except Exception as e: print(f"Error opening image {image_path} for aircraft {aircraft_name}: {e}")
 
-def show_list_of_aircraft(selected_aircraft,text_size):
+def show_list_of_aircraft(selected_aircraft, text_size):
     def on_aircraft_click(event):
-        photo_references = []
         index = listbox.nearest(event.y)
-        with open("paths.txt") as file:
-            paths = file.readlines()
-            if 0 <= index < len(paths):
-                pather = paths[index].strip()  # Get the path at the clicked index
-                selected_aircraft_name = selected_aircraft[index]  # Get the aircraft name
-                open_image(photo_references, pather, selected_aircraft_name)
+        if 0 <= index < len(selected_aircraft):
+            selected_aircraft_name = selected_aircraft[index]  # Get the aircraft name
+            with open("paths.txt") as file:
+                paths = [path.strip() for path in file.readlines()]
+                if index < len(paths):
+                    pather = paths[index]  # Get the path at the clicked index
+                    photo_references = []
+                    open_image(photo_references, pather, selected_aircraft_name)
+                else:
+                    print(f"No path found for aircraft {selected_aircraft_name}")
 
     root = tk.Tk()
     root.title("List of Selected Aircraft")
@@ -159,7 +206,7 @@ def show_list_of_aircraft(selected_aircraft,text_size):
     listbox.pack(fill=tk.BOTH, expand=tk.YES)
     
     for i, aircraft in enumerate(selected_aircraft, start=1):
-        listbox.insert(tk.END, f"{i}. {aircraft}")  # Include the index and full aircraft name
+        listbox.insert(tk.END, f"{i}. {aircraft}")  #index and full aircraft name for the answer list
     
     listbox.bind('<Double-1>', on_aircraft_click)  # Bind double click event to callback
     root.mainloop()
