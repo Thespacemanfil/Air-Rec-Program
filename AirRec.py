3cbb96b391a423af5cc6f514e3bdf1dcfb3afd01
Thespacemanfil <97069887+Thespacemanfil@users.noreply.github.com>
2024-02-06 18:00:23 +0000

    Features
    Press enter to continue
    Slide number
    Custom mode bugfix
    Updated exe

--- AirRec.py
+++ AirRec.py
@@ -34,10 +34,11 @@ def slideshowmenu(settings):
             })
         case "learn":
             settings.update({
-                "slideshow_length": 10,
-                "slideshow_time": 999,
+                "slideshow_length": -1,
+                "slideshow_time": 9999,
                 "instant_reveal": True,
                 "timer": False,
+                "txt_file": input("Which list of aircraft do you want to draw from? " + str(glob.glob("*.txt")) + "\n"),
                 "extension": " aircraft",
             })
         case "test":
@@ -51,7 +52,7 @@ def slideshowmenu(settings):
             if input("Reveal answers immediately yes/no\n").lower() == "yes": instant_reveal = True
             else: instant_reveal = False
             if input("Visible countdown timer yes/no\n").lower() == "yes": timer = True
-            else: instant_reveal = False
+            else: timer = False
             settings.update({
                 "slideshow_length": int(input("Amount of slides? -1 slides will run through all aircraft in the list\n")),
                 "slideshow_time": int(input("Seconds per slide?\n")),
@@ -68,6 +69,8 @@ def slideshowmenu(settings):
 def slideshow(path,slideshow_length,slideshow_time,instant_reveal,intermission_time,variance,txt_file,text_size,extension,timer):
     selected_aircraft = aircraft_selector(txt_file,slideshow_length)
     image_downloader(selected_aircraft,extension,path,variance)
+    print("-------------------------------------------------------------------------------------------------------------")
+    input("Press enter to continue: ")
     run_slideshow(slideshow_time,path,text_size,timer,instant_reveal,selected_aircraft,intermission_time,extension)
     show_list_of_aircraft(selected_aircraft,text_size)
     menu()
@@ -92,11 +95,12 @@ def image_downloader(selected_aircraft, extension, path, variance):
             if num_files_before < len(os.listdir(output_path)): break
         else: print(f"Failed to download image for {query} after 3 attempts.")
         
-def show_image(remaining_time,timer,instant_reveal,text_size,filename,image_path,intermission,intermission_time):
+def show_image(remaining_time,timer,instant_reveal,text_size,filename,image_path,intermission,intermission_time,slide_num):
     root = tk.Tk()
     root.title("Aircraft Image")
     w, h = root.winfo_screenwidth(), root.winfo_screenheight()
     root.geometry(f"{w}x{h}")  
+    root.attributes("-topmost", True)
     if intermission: root.configure(bg='black')
     
     def close_window():
@@ -111,6 +115,7 @@ def show_image(remaining_time,timer,instant_reveal,text_size,filename,image_path
         
     timer_label = ttk.Label(root, text=str(remaining_time), font=('Arial', text_size), foreground='orange')
     aircraft_label = ttk.Label(root, text=filename, font=('Arial', text_size), foreground='white', background='black')
+    slide_label = ttk.Label(root, text=slide_num, font=('Arial', text_size), foreground='black', background='white')
     
     def place_labels():
         root.update()  # Update the window to get the correct dimensions
@@ -126,6 +131,9 @@ def show_image(remaining_time,timer,instant_reveal,text_size,filename,image_path
                 aircraft_label.place(x=w/2, y=35, anchor="center")
         else:
             aircraft_label.place_forget()
+
+        if not intermission:
+            slide_label.place(x=w/20, y=35, anchor="nw")
     
     def update_timer():
         nonlocal remaining_time  # Use nonlocal to modify the outer variable
@@ -141,6 +149,7 @@ def show_image(remaining_time,timer,instant_reveal,text_size,filename,image_path
     root.mainloop()
 
 def run_slideshow(slideshow_time, path, text_size, timer, instant_reveal, selected_aircraft, intermission_time, extension):
+    slide_num = 1
     for aircraft in selected_aircraft:
         folder_path = os.path.join(path, aircraft + extension) # get the folder path for each aircraft
         images = [f for f in os.listdir(folder_path) if os.path.splitext(f)[1] in (".png", ".jpg", ".jpeg")] # list the image files in the folder
@@ -152,10 +161,12 @@ def run_slideshow(slideshow_time, path, text_size, timer, instant_reveal, select
         with open("paths.txt", "a") as f:
             f.write(image_path + "\n")
         
-        show_image(slideshow_time, timer, instant_reveal, text_size, aircraft, image_path, False, intermission_time)
+        show_image(slideshow_time, timer, instant_reveal, text_size, aircraft, image_path, False, intermission_time,slide_num)
         
         if intermission_time > 0: 
-            show_image(intermission_time, timer, instant_reveal, text_size, aircraft, image_path, True, intermission_time)
+            show_image(intermission_time, timer, instant_reveal, text_size, aircraft, image_path, True, intermission_time,"")
+
+        slide_num += 1
 
 def open_image(photo_references, image_path, aircraft_name):
     try:
