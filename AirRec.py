e78b621534a1657a42c57eb10d2bdd70cd545a6d
fil <97069887+Thespacemanfil@users.noreply.github.com>
2024-03-19 15:06:49 +0000

    code re-write

--- AirRec.py
+++ AirRec.py
@@ -1,4 +1,4 @@
-import glob, os, random, time, msvcrt, requests
+import glob, os, shutil, random, time, msvcrt, requests
 from PIL import Image, ImageTk #pillow
 import tkinter as tk
 from tkinter import ttk
@@ -17,9 +17,11 @@ def crash(reason):
     os._exit(0)
 
 def menu():
-    error()
+    path = os.path.dirname(os.path.realpath(__file__))
+    if os.path.exists(path) and os.path.isdir(path): os.remove(path)
+
     settings = {
-        "path": "C:/aircraft_recognition_program/images/",
+        "path": f"{path}/images",
         "txt_file": get_txt("default.txt"),
         "slideshow_length": 30,
         "slideshow_time": 10,
@@ -49,10 +51,10 @@ def mode_choices(settings):
         case "learn":
             settings.update({
                 "slideshow_length": -1,
-                "slideshow_time": 9999,
+                "slideshow_time": -1,
                 "instant_reveal": True,
                 "timer": False,
-                "txt_file": get_txt(),
+                "txt_file": get_txt(None),
                 "extension": " aircraft",
             })
             return True
@@ -68,7 +70,7 @@ def mode_choices(settings):
             settings.update({
                 "txt_file": get_txt(None),
                 "slideshow_length": get_int("Slide count (-1 will use entire list):\n"),
-                "slideshow_time": get_int("Slide length (seconds):\n"),
+                "slideshow_time": get_int("Slide length (seconds, -1 for unlimited):\n"),
                 "instant_reveal": get_yn("Reveal answers immediately y/n"),
                 "intermission_time": get_int("intermission length (seconds):\n"),
                 "extension": (" " + input("Search modifier: e.g top view, in flight, [or leave blank]\n")).rstrip(),
@@ -100,11 +102,11 @@ def get_int(text):
 
 def slideshow(path,slideshow_length,slideshow_time,instant_reveal,intermission_time,variance,txt_file,text_size,extension,timer):
     selected_aircraft = aircraft_selector(txt_file,slideshow_length)
-    image_downloader(selected_aircraft,extension,path,variance)
+    selected_aircraft, selected_paths = image_downloader(selected_aircraft,extension,path,variance)
     print("\n---------------------------------------------------------------------------------\nPress any key to continue")
     msvcrt.getch()
-    paths = run_slideshow(slideshow_time,path,text_size,timer,instant_reveal,selected_aircraft,intermission_time,extension)
-    show_list_of_aircraft(selected_aircraft,text_size,paths)
+    run_slideshow(slideshow_time, selected_paths, text_size, timer, instant_reveal, selected_aircraft, intermission_time)
+    show_list_of_aircraft(selected_aircraft,text_size,selected_paths)
     menu()
 
 def aircraft_selector(txt_file,slideshow_length):
@@ -118,88 +120,97 @@ def aircraft_selector(txt_file,slideshow_length):
     else: print("INVALID SLIDESHOW LENGTH")
 
 def image_downloader(selected_aircraft, extension, path, variance):
+    selected_paths = []
     for aircraft in selected_aircraft:
         query = aircraft + extension
         output_path = os.path.join(path, query)
-        for _ in range(3):  # Try to download the image
-            num_files_before = len(os.listdir(output_path)) if os.path.exists(output_path) else 0
-            downloader.download(query, limit=variance, output_dir=path, adult_filter_off=False, force_replace=False, timeout=10, filter="photo", verbose=False)
-            if num_files_before < len(os.listdir(output_path)): break
-        else:
-            print(f"Failed to download image for {query} after 3 attempts.")
+
+        downloader.download(query, limit=variance, output_dir=path, adult_filter_off=False, force_replace=False, timeout=10, filter="photo", verbose=False)
+        
+        if len(glob.glob(f'{output_path}/*')) == 0 or not os.path.exists(output_path):
+            print(f"Failed to download image for {query}")
             selected_aircraft.remove(aircraft)
-        
-def show_image(remaining_time,timer,instant_reveal,text_size,filename,image_path,intermission,intermission_time,slide_num):
-    root = tk.Tk()
-    root.title("Aircraft Image")
-    w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-    root.geometry(f"{w}x{h}")  
-    root.attributes("-topmost", True)
-    if intermission: root.configure(bg='black')
-    
-    def close_window():
-        root.destroy()
-
-    if not intermission:
-        img = Image.open(image_path)
-        resize_image = img.resize((w, h))
-        photo = ImageTk.PhotoImage(resize_image)
-        label = ttk.Label(root, image=photo)
-        label.pack(fill=tk.BOTH, expand=tk.YES)
-        
-    timer_label = ttk.Label(root, text=str(remaining_time), font=('Arial', text_size), foreground='orange')
-    aircraft_label = ttk.Label(root, text=filename, font=('Arial', text_size), foreground='white', background='black')
-    slide_label = ttk.Label(root, text=slide_num, font=('Arial', text_size), foreground='black', background='white')
-    
-    def place_labels():
-        root.update()  # Update the window to get the correct dimensions
-        if timer:
-            timer_label.place(x=root.winfo_width()-100, y=20)
         else:
-            timer_label.place_forget()
-            
-        if instant_reveal:
-            if intermission:
+            images = [f for f in os.listdir(output_path) if os.path.splitext(f)[1] in (".png", ".jpg", ".jpeg")] # list the image files in the folder   
+            selected_paths.append(os.path.join(output_path, random.choice(images)))
+    
+    print(f"selected aircraft:{selected_aircraft}\nselected paths:{selected_paths}")
+    return selected_aircraft, selected_paths
+        
+class display_image:
+    def __init__(self, remaining_time, timer, instant_reveal, text_size, filename, image_path, intermission, intermission_time, slide_num):
+        self.root = tk.Tk()
+        self.root.title("Aircraft Image")
+        w, h = self.root.winfo_screenwidth(), self.root.winfo_screenheight()
+        self.root.geometry(f"{w}x{h}")
+        self.root.attributes("-topmost", True)
+
+        def close_window():
+            self.root.destroy()
+
+        if intermission:
+            self.root.configure(bg='black')
+        else:
+            img = Image.open(image_path)
+            resize_image = img.resize((w, h))
+            photo = ImageTk.PhotoImage(resize_image)
+            label = ttk.Label(self.root, image=photo)
+            label.pack(fill=tk.BOTH, expand=tk.YES)
+
+        timer_label = ttk.Label(self.root, text=str(remaining_time), font=('Arial', text_size), foreground='orange')
+        aircraft_label = ttk.Label(self.root, text=filename, font=('Arial', text_size), foreground='white', background='black')
+        slide_label = ttk.Label(self.root, text=slide_num, font=('Arial', text_size), foreground='black', background='white')
+
+        def place_labels():
+            self.root.update()  # Update the window to get the correct dimensions
+
+        if intermission:
+            if timer and intermission_time > 0:
+                timer_label.place(x=w - 100, y=20)
+            else:
+                timer_label.place_forget()
+
+            if instant_reveal:
                 aircraft_label.place(x=w/2, y=h/2, anchor="center")
-            elif intermission_time == 0:
-                aircraft_label.place(x=w/2, y=35, anchor="center")
+            else:
+                aircraft_label.place_forget()
+
         else:
-            aircraft_label.place_forget()
+            if timer and remaining_time > 0:
+                timer_label.place(x=w - 100, y=20)
+            else:
+                timer_label.place_forget()
 
-        if not intermission:
-            slide_label.place(x=w/20, y=35, anchor="nw")
-    
-    def update_timer():
-        nonlocal remaining_time
-        remaining_time -= 1
-        timer_label.config(text=str(remaining_time))
-        root.after(1000, update_timer)
-    
-    root.after((remaining_time*1000), close_window)
-
-    place_labels()
-    update_timer()
-
-    root.mainloop()
-
-def run_slideshow(slideshow_time, path, text_size, timer, instant_reveal, selected_aircraft, intermission_time, extension):
-    slide_num = 1
-    paths = []
-    for aircraft in selected_aircraft:
-        folder_path = os.path.join(path, aircraft + extension) # get the folder path for each aircraft
-        images = [f for f in os.listdir(folder_path) if os.path.splitext(f)[1] in (".png", ".jpg", ".jpeg")] # list the image files in the folder
-        if not images: 
-            print(f"Image not found: {aircraft}")
-            continue
-
-        image_path = os.path.join(folder_path, random.choice(images)) # get the full image path
-        paths.append(image_path)
+            if instant_reveal and intermission_time == 0:
+                aircraft_label.place(x=w/2, y=35, anchor="center")
+            else:
+                aircraft_label.place_forget()
         
-        show_image(slideshow_time, timer, instant_reveal, text_size, aircraft, image_path, False, intermission_time,slide_num)
-        if intermission_time > 0: show_image(intermission_time, timer, instant_reveal, text_size, aircraft, image_path, True, intermission_time,"")
+        slide_label.place(x=w/20, y=35, anchor="nw")
 
-        slide_num += 1
-    return paths
+        def update_timer():
+            nonlocal remaining_time
+            remaining_time -= 1
+            timer_label.config(text=str(remaining_time))
+            self.root.after(1000, update_timer)
+
+        place_labels()
+
+        if remaining_time != -1:
+            self.root.after((remaining_time*1000), close_window)
+            update_timer()
+
+        self.root.mainloop()
+        
+def run_slideshow(slideshow_time, selected_paths, text_size, timer, instant_reveal, selected_aircraft, intermission_time):
+    i = 0
+    for aircraft in selected_aircraft:
+        image_path = selected_paths[i]
+        slide_num = i + 1
+        i = slide_num
+        
+        show_image = display_image(slideshow_time, timer, instant_reveal, text_size, aircraft, image_path, False, intermission_time,slide_num)
+        if intermission_time > 0: show_image = display_image(intermission_time, timer, instant_reveal, text_size, aircraft, image_path, True, intermission_time,"")
 
 def open_image(image_path, aircraft_name):
     try:
