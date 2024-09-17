15b6804ad33fc82fc8c206c24550d5a3a27eec8d
fil <97069887+Thespacemanfil@users.noreply.github.com>
2024-09-17 16:56:32 +0100

    Update AirRec.py

--- AirRec.py
+++ AirRec.py
@@ -9,7 +9,7 @@ class HiddenPrints:
         self._original_stdout = sys.stdout
         sys.stdout = open(os.devnull, 'w')
 
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout.close()
         sys.stdout = self._original_stdout
 
